7d1a9dbd02f67f635d93a80392d6cb5dce29db3c
fyx-ssj <huoyingcanai@outlook.com>
2021-05-21 10:15:25 +0800

    更新163邮箱登录代码

--- Python登录各大主流网站/163email/163email.py
+++ Python登录各大主流网站/163email/163email.py
@@ -11,13 +11,14 @@ def login():
     driver = webdriver.Chrome()
     url = 'http://mail.163.com/'
     driver.get(url)
-
-    # 等待页面加载完成，出现可以点击到密码登录的button
+"""
+    # 如果打开页面默认为扫码登录页面时，需要等待页面加载完成，出现可以点击到密码登录的button
     wait = WebDriverWait(driver, 10)
     wait.until(EC.element_to_be_clickable((By.ID, 'lbNormal')))
     driver.find_element_by_id('lbNormal').click()
+"""
 
-
+    #切换到指定iframe是本次登录的重重之中
     # 使用CSSSelector正则匹配头部
     elem = driver.find_element_by_css_selector("iframe[id^='x-URS-iframe']")
     # 163登陆框是使用iframe进行嵌套的，所以需要先切换到该iframe
